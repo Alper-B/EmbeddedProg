0936678299cc7f2f96d63a14b1ba3875ae7b9ec5
Barutca Alper <alper.barutca@aalto.fi>
2026-01-30 04:13:31 +0200

    slm

--- Module_2/T2_5_Bank_account/src/bank_account.cpp
+++ Module_2/T2_5_Bank_account/src/bank_account.cpp
@@ -1,4 +1,51 @@
-
 #include "bank_account.hpp"
+#include <iostream>
+
+// Constructor
+BankAccount::BankAccount(std::string const& owner,
+                         std::string const& account_number,
+                         double balance)
+    : owner_(owner), account_number_(account_number), balance_(balance) {}
+
+// Accessors
+std::string BankAccount::GetOwner() const {
+    return owner_;
+}
+
+std::string BankAccount::GetNumber() const {
+    return account_number_;
+}
+
+double BankAccount::GetBalance() const {
+    return balance_;
+}
+
+// Modifiers / operations
+void BankAccount::Deposit(double amount) {
+    balance_ += amount;
+}
+
+bool BankAccount::Withdraw(double amount) {
+    if (amount > balance_) {
+        return false;
+    }
+    balance_ -= amount;
+    return true;
+}
+
+void BankAccount::Print() const {
+    std::cout << "Account num: " << account_number_
+              << ", owned by: " << owner_
+              << ", balance: " << balance_ << " eur." << std::endl;
+}
 
 
+bool Transfer(BankAccount& source_account, BankAccount& target_account, double amount) {
+
+    if (!source_account.Withdraw(amount)) {
+        return false;
+    }
+
+    target_account.Deposit(amount);
+    return true;
+}
