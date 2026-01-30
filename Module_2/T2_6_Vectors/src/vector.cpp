0f2b491184f7418ab7efa1b08b4ea8892a5f8c46
Barutca Alper <alper.barutca@aalto.fi>
2026-01-30 04:16:29 +0200

    slm

--- Module_2/T2_6_Vectors/src/vector.cpp
+++ Module_2/T2_6_Vectors/src/vector.cpp
@@ -1,18 +1,24 @@
 #include "vector.hpp"
-
 #include <vector>
 
 int GetMin(std::vector<int> v) {
-  
-  return 0; 
+    int min_val = v[0];
+    for (int x : v) {
+        if (x < min_val) min_val = x;
+    }
+    return min_val;
 }
 
 int GetMax(std::vector<int> v) {
-  
-  return 0; 
+    int max_val = v[0];
+    for (int x : v) {
+        if (x > max_val) max_val = x;
+    }
+    return max_val;
 }
 
 double GetAvg(std::vector<int> v) {
-  
-  return 0.0; 
+    long long sum = 0;
+    for (int x : v) sum += x;
+    return static_cast<double>(sum) / static_cast<double>(v.size());
 }
