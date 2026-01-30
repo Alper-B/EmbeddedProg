c6f33ad4df044d94f0ac33365da0af040815ca1e
Barutca Alper <alper.barutca@aalto.fi>
2026-01-30 03:59:47 +0200

    slm

--- Module_2/T2_3_Simple_class/src/person.cpp
+++ Module_2/T2_3_Simple_class/src/person.cpp
@@ -2,3 +2,14 @@
 
 // define your Person class' functions here
 
+
+Person::Person(const std::string& name, int year) : Name(name), birthyear(year) {}
+
+std::string Person::GetName() const {
+    return Name;
+}
+
+int Person::GetAge(int year) const {
+    return year - birthyear;
+}
+
--- Module_2/T2_3_Simple_class/src/person.hpp
+++ Module_2/T2_3_Simple_class/src/person.hpp
@@ -3,5 +3,21 @@
 
 // declare your Person class here
 
+#ifndef PERSON_HPP
+#define PERSON_HPP
+
+#include <string>
+
+class Person {
+private:
+    std::string Name;
+    int birthyear;
+
+public:
+    Person(const std::string& name, int year); // constructor
+
+    std::string GetName() const;  // returns the name
+    int GetAge(int year) const;   // returns age in a given year
+};
 
 #endif
