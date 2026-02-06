b27e307b57e17ff78bd0cad5896805f7130c2b8c
Barutca Alper <alper.barutca@aalto.fi>
2026-02-06 02:33:07 +0200

    hi

--- Module_3/T3_2_List/src/list.cpp
+++ Module_3/T3_2_List/src/list.cpp
@@ -1,17 +1,27 @@
 #include "list.hpp"
 
 #include <iostream>
-
-// Implement the functions here
+#include <list>
+#include <string>
 
 std::istream& GetLines(std::istream& is, std::list<std::string>& list) {
-  
+    list.clear();
+
+    std::string line;
+    while (std::getline(is, line)) {
+        list.push_back(line);
+    }
+
+    return is;
 }
 
 void Print(const std::list<std::string>& list) {
-  
+    for (const auto& line : list) {
+        std::cout << line << std::endl;
+    }
 }
 
 void SortAndUnique(std::list<std::string>& list) {
-  
+    list.sort();
+    list.unique();
 }
