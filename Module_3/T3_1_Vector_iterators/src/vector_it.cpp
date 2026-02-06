35b71ffa8a1370ae804d28e8fec7aace75912069
Barutca Alper <alper.barutca@aalto.fi>
2026-02-06 02:21:33 +0200

    hi

--- Module_3/T3_1_Vector_iterators/src/vector_it.cpp
+++ Module_3/T3_1_Vector_iterators/src/vector_it.cpp
@@ -1,45 +1,46 @@
 #include "vector_it.hpp"
+
 #include <iostream>
 #include <vector>
 
 std::vector<int> ReadVector() {
     std::vector<int> v;
     int value;
-    
+
     while (std::cin >> value) {
         v.push_back(value);
     }
-    
+
     return v;
 }
 
 void PrintSum1(const std::vector<int>& v) {
-
     if (v.size() < 2) {
-        std::cout << "\n";
+        std::cout << std::endl;
         return;
     }
 
-    for (auto it = v.begin(); it != v.end() - 1; ++it) {
+    for (auto it = v.begin(); it + 1 != v.end(); ++it) {
         std::cout << *it + *(it + 1) << " ";
     }
-    std::cout << "\n";
+
+    std::cout << std::endl;
 }
 
 void PrintSum2(const std::vector<int>& v) {
     if (v.empty()) {
-        std::cout << "\n";
+        std::cout << std::endl;
         return;
     }
 
-    auto beg = v.begin();
-    auto end = v.end() - 1;
+    auto front = v.begin();
+    auto back = v.end() - 1;
 
-    while (beg < end) {
-        std::cout << *beg + *end << " ";
-        
-        ++beg;
-        --end;
+    while (front <= back) {
+        std::cout << *front + *back << " ";
+        ++front;
+        --back;
     }
-    std::cout << "\n";
+
+    std::cout << std::endl;
 }
