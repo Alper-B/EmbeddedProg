5653601c39b0724a63a56d5d8794bcad74194360
Barutca Alper <alper.barutca@aalto.fi>
2026-02-06 02:17:22 +0200

    hi

---
+++ Module_3/T3_1_Vector_iterators/src/vector_it.cpp
@@ -0,0 +1,44 @@
+#include "vector_it.hpp"
+#include <iostream>
+#include <vector>
+
+std::vector<int> ReadVector() {
+    std::vector<int> v;
+    int value;
+    
+    while (std::cin >> value) {
+        v.push_back(value);
+    }
+    
+    return v;
+}
+
+void PrintSum1(const std::vector<int>& v) {
+    if (v.size() < 2) {
+        std::cout << "\n";
+        return;
+    }
+
+    for (auto it = v.begin(); it != v.end() - 1; ++it) {
+        std::cout << *it + *(it + 1) << " ";
+    }
+    std::cout << "\n";
+}
+
+void PrintSum2(const std::vector<int>& v) {
+    if (v.empty()) {
+        std::cout << "\n";
+        return;
+    }
+
+    auto beg = v.begin();
+    auto end = v.end() - 1;
+
+    while (beg <= end) {
+        std::cout << *beg + *end << " ";
+        
+        ++beg;
+        --end;
+    }
+    std::cout << "\n";
+}
