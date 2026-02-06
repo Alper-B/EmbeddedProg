960f5b127a3aedac99dbc762664bda59e82253d8
Barutca Alper <alper.barutca@aalto.fi>
2026-02-06 02:36:53 +0200

    hi

--- Module_3/T3_3_Matrix_rotation/src/matrix.cpp
+++ Module_3/T3_3_Matrix_rotation/src/matrix.cpp
@@ -3,15 +3,40 @@
 #include <iostream>
 #include <vector>
 
-// Implement the functions here
-// Remember to use auto and ranged for-loops when they can be used!
-
 // Assumes 'm' is a square matrix
 Matrix ReadMatrix(int n) {
-  }
+    Matrix m(n, std::vector<int>(n));
 
-Matrix Rotate90Deg(const Matrix &m) {
-  }
+    for (auto& row : m) {
+        for (auto& elem : row) {
+            std::cin >> elem;
+        }
+    }
 
-void Print(const Matrix &m) {
-  }
+    return m;
+}
+
+Matrix Rotate90Deg(const Matrix& m) {
+    int n = m.size();
+    Matrix rotated(n, std::vector<int>(n));
+
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            rotated[j][n - 1 - i] = m[i][j];
+        }
+    }
+
+    return rotated;
+}
+
+void Print(const Matrix& m) {
+    int n = m.size();
+    std::cout << "Printing out a " << n << " x " << n << " matrix:" << std::endl;
+
+    for (const auto& row : m) {
+        for (const auto& elem : row) {
+            std::cout << elem << " ";
+        }
+        std::cout << std::endl;
+    }
+}
